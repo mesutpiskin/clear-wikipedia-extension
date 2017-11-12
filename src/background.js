2aa7cb9488398c743d654946764e24ca2d91de40
RamazanM <r.ramazanmutlu@gmail.com>
2017-11-13 00:00:01 +0300

    Bug giderildi, if sayısı azaltıldı.

--- src/background.js
+++ src/background.js
@@ -1,24 +1,10 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  
-   var url=changeInfo.url;
-   var newUrl;
-   if(url.search("https://tr.wikipedia") != -1 ){
-	  newUrl=url.replace("https://tr.wikipedia", "https://tr.0wikipedia");	  	   
-   }
-   else if (url.search("https://en.wikipedia") != -1)
-   {
-	     newUrl=url.replace("https://en.wikipedia", "https://en.0wikipedia");
-   }
-   else
-	{
-	   return;
-	}
-	 
-	chrome.tabs.create({url: newUrl});
-})
-
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.tabs.get(activeInfo.tabId, function(tab){
-     console.log(tab.url);
-  });
-}); 
+  var url = tab.url;
+  if (url.search(".wikipedia") != -1) {
+    newUrl = url.replace(".wikipedia", ".0wikipedia");
+    chrome.tabs.update(tabId, {url: newUrl});
+  }
+  else {
+    return;
+  }
+});
