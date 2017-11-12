3b861a3d25d43f5d7c1f40d0fb6135ff1c12c489
RamazanM <r.ramazanmutlu@gmail.com>
2017-11-13 02:14:24 +0300

    Opera support...

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   var url = tab.url;
-  if (url.search(".wikipedia") != -1 && url.search(".0wikipedia") == -1) {
+  if (url.search(".wikipedia.org/") != -1 && url.search(".0wikipedia") == -1) {
     newUrl = url.replace(".wikipedia", ".0wikipedia");
     chrome.tabs.update(tabId, {url: newUrl});
   }
@@ -10,10 +10,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 // mesut=document.getElementById("mesut");
-// mesut.on
-//
+// mesut.onclick=openInNewTab(0);
+// ramazan=document.getElementById("ramazan");
+// ramazan.onclick=openInNewTab(1);
+
 // function openInNewTab(id) {
-//   links={"http://www.mesutpiskin.com","https://github.com/RamazanM"};
+//   links[2]={"http://www.mesutpiskin.com","https://github.com/RamazanM"};
 //   alert();
 //   var win = window.open(links[id], '_blank');
 //   win.focus();
