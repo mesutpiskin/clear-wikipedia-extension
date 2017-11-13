86bb7486fafcd10dba14084d0844f81ec903e8a0
Can Yuva <canyuva06@gmail.com>
2017-11-13 19:09:12 +0300

    support on tr vikipedi urls

--- src/background.js
+++ src/background.js
@@ -1,10 +1,14 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   var url = tab.url;
-  if (url.search(".wikipedia.org/") != -1 && url.search(".0wikipedia") == -1) {
-    newUrl = url.replace(".wikipedia", ".0wikipedia");
+  if (url.search(".wikipedia.org") != -1 && url.search(".0wikipedia.org") == -1) {
+    newUrl = url.replace(".wikipedia.org", ".0wikipedia.org");
     chrome.tabs.update(tabId, {url: newUrl});
   }
-  else {
-    return;
+  else if (url.search(".vikipedi.org") != -1 && url.search(".0vikipedi.org") == -1) {
+    newUrl = url.replace(".vikipedi.org", ".0vikipedi.org");
+    chrome.tabs.update(tabId, {url: newUrl});
+  }
+  else{
+	  return;
   }
 });
